426f209e0be6a600def84ba400ec505981a17567
AbdelmoulaElAmrani <elamraniabdelmoula@gmail.com>
2019-01-17 13:46:25 -0600

    spring setup

---
+++ src/main/java/com/codeup/ticketsup/TicketsupApplication.java
@@ -0,0 +1,14 @@
+package com.codeup.ticketsup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketsupApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/codeup/ticketsup/TicketsupApplicationTests.java
@@ -0,0 +1,17 @@
+package com.codeup.ticketsup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TicketsupApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
