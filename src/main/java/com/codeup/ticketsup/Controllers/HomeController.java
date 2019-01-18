52b3b92ec76df1683c38e8c9130cf99f490b1646
AbdelmoulaElAmrani <elamraniabdelmoula@gmail.com>
2019-01-18 16:32:35 -0600

    test the landing page

--- src/main/java/com/codeup/ticketsup/Controllers/HomeController.java
+++ src/main/java/com/codeup/ticketsup/Controllers/HomeController.java
@@ -2,13 +2,12 @@ package com.codeup.ticketsup.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "index.html";
+    @GetMapping("/")
+    public String home() {
+        return "index";
     }
 }
