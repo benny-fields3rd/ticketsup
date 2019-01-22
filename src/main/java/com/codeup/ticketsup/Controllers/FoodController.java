d2cb9cb79f72b4f71a0b95eb08f07b2ac9309b3c
Benny Fields <benny.fields3rd@gmail.com>
2019-01-22 14:29:04 -0600

    test

--- src/main/java/com/codeup/ticketsup/Controllers/FoodController.java
+++ src/main/java/com/codeup/ticketsup/Controllers/FoodController.java
@@ -1,4 +1,14 @@
 package com.codeup.ticketsup.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class FoodController {
+
+    @GetMapping("/food")
+
+    public String movie() {
+        return "ticketOrders/food";
+    }
 }
