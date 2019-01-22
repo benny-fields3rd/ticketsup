701b6073ea2708b80f3cd694d2c7f06a7288c410
Karen Rivas <karen.rivas0410@gmail.com>
2019-01-21 18:58:52 -0600

    added api to single movie page

--- src/main/java/com/codeup/ticketsup/Controllers/MovieController.java
+++ src/main/java/com/codeup/ticketsup/Controllers/MovieController.java
@@ -1,27 +1,24 @@
 package com.codeup.ticketsup.Controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.ArrayList;
 
 @Controller
 public class MovieController {
-    @GetMapping("/order")
-    public String index(Model model) {
-        model.addAttribute("posts",postService.all());
-        return "posts/index";
+
+    @GetMapping("/movie")
+
+    public String movie() {
+        return "ticketOrders/moviepage";
     }
 
-    g("/posts")
-    public String index(Model model) {
-        posts = new ArrayList<>();
-        posts.add(new Post("New Post 1", "lorem impsum dolor set amit",1));
-        posts.add(new Post("New Post 2", "lorem impsum dolor set amit", 2));
-        posts.add(new Post("New Post 3", "lorem impsum dolor set amit", 3));
-        posts.add(new Post("New Post 4", "lorem impsum dolor set amit", 4));
-        model.addAttribute("posts",posts);
-        return "posts/index";
-    }
+//    @GetMapping("/movie/{id}")
+//    public String showMovie(@PathVariable int id, int omdb_id) {
+//
+//        return "ticketOrders/moviepage";
+//
+//    }
+
+
+
 }
