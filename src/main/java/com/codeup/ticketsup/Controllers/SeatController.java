1c33b5bfe318a78e5c9a31fadf28943ff33c7097
Karen Rivas <karen.rivas0410@gmail.com>
2019-01-23 10:31:04 -0600

    initial add

--- src/main/java/com/codeup/ticketsup/Controllers/SeatController.java
+++ src/main/java/com/codeup/ticketsup/Controllers/SeatController.java
@@ -1,4 +1,13 @@
 package com.codeup.ticketsup.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class SeatController {
+
+    @GetMapping("/seat-selection")
+    public String seat() {
+        return "seat_selection";
+    }
 }
