5d796ac07f8a83168e3c26c84ce02cdb0c21bc22
Karen Rivas <karen.rivas0410@gmail.com>
2019-01-24 11:38:22 -0600

    working on seat selection

--- src/main/java/com/codeup/ticketsup/Controllers/SeatController.java
+++ src/main/java/com/codeup/ticketsup/Controllers/SeatController.java
@@ -1,14 +1,23 @@
 package com.codeup.ticketsup.Controllers;
 
+import com.codeup.ticketsup.interfaces.SeatRepository;
+import com.codeup.ticketsup.models.Order;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SeatController {
+    private SeatRepository sp;
+//    private
 
     @GetMapping("/seat-selection")
-
-    public String pickSeat() {
+    public String selectSeat() {
         return "user/seat_selection";
+
     }
+
+
 }
--- src/main/java/com/codeup/ticketsup/interfaces/SeatRepository.java
+++ src/main/java/com/codeup/ticketsup/interfaces/SeatRepository.java
@@ -4,4 +4,5 @@ import com.codeup.ticketsup.models.Seat;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SeatRepository extends CrudRepository<Seat, Integer> {
+
 }
