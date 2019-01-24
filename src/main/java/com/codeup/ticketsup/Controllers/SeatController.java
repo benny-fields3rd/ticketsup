1397093265a245e3ce8c67ec2b94a7a8704915c2
Karen Rivas <karen.rivas0410@gmail.com>
2019-01-24 11:40:26 -0600

    working on seat selection

--- src/main/java/com/codeup/ticketsup/Controllers/SeatController.java
+++ src/main/java/com/codeup/ticketsup/Controllers/SeatController.java
@@ -1,12 +1,10 @@
 package com.codeup.ticketsup.Controllers;
 
 import com.codeup.ticketsup.interfaces.SeatRepository;
-import com.codeup.ticketsup.models.Order;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 public class SeatController {
