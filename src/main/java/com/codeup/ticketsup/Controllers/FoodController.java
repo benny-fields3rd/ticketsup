d6d08e4e762f77d208450a2d3dfb3fa9d2ea790e
abdelmoula el amrani <elamraniabdelmoula@gmail.com>
2019-01-25 16:03:05 -0600

    food controller data

--- src/main/java/com/codeup/ticketsup/Controllers/FoodController.java
+++ src/main/java/com/codeup/ticketsup/Controllers/FoodController.java
@@ -1,12 +1,15 @@
 package com.codeup.ticketsup.Controllers;
 
+import com.codeup.ticketsup.interfaces.FoodRepository;
 import com.codeup.ticketsup.interfaces.OrderRepository;
 import com.codeup.ticketsup.models.Food;
 import com.codeup.ticketsup.models.Order;
 import com.codeup.ticketsup.models.Seat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,15 +20,19 @@ public class FoodController {
 
     @Autowired
     OrderRepository orderRepository;
+    @Autowired
+    FoodRepository foodRepository;
 
     @GetMapping("/food/{id}")
 
-    public String movie() {
+    public String food(@PathVariable int id, Model model) {
+        model.addAttribute("order_id", id);
+        model.addAttribute("food" ,  foodRepository.findAll());
         return "ticketOrders/food";
     }
 
     @PostMapping("/food")
-    public String saveSeats(@RequestParam(name = "order_id") int id, @RequestParam(name = "food") List<Food> food) {
+    public String saveFood(@RequestParam(name = "order_id") int id, @RequestParam(name = "food") List<Food> food) {
         Order order = orderRepository.findOne(id);
         order.setFood(food);
         orderRepository.save(order);
