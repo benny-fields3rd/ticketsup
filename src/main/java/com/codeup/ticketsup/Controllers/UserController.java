3f76d8db17ba0c059e94391c4781289518d86a97
Karen Rivas <karen.rivas0410@gmail.com>
2019-01-29 09:08:20 -0600

    added edit user

--- src/main/java/com/codeup/ticketsup/Controllers/UserController.java
+++ src/main/java/com/codeup/ticketsup/Controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -37,4 +38,11 @@ public class UserController {
         return "redirect:/login";
     }
 
+    @GetMapping("/users/{id}/edit")
+    public String showEditForm(@PathVariable Long id, Model viewModel){
+        User user = usersRepository.getOne(id);
+        viewModel.addAttribute("user", user);
+        viewModel.addAttribute("showEditControls", usersService.canEditProfile(user));
+        return "users/edit";
+    }
 }
